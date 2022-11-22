49623ee3cd35a3f48ff2d89bee6ce86dc64559bb
radiobenzene <aurangabadkaruditangshu@gmail.com>
2022-11-22 11:56:47 +0000

    Add function to read audio track

--- Median_Flter_Restoration.py
+++ Median_Flter_Restoration.py
@@ -1,1 +1,25 @@
 #Author - Uditangshu Aurangabadkar
+import wave
+import matplotlib.pyplot as plot
+
+#Function to read an audio track
+'''
+    Params - track_name - specifies the name of the track to be read
+    Return - track that has been read as an object type
+'''
+def readTrack(track_name):
+    obj = wave.open(track_name, 'r')
+    return obj
+
+#Function to plot an audio track frequency
+'''
+    Params - The audio track
+    Return - A plot of the audio track
+'''
+def plotTrack(track, fig):
+    pass
+track = readTrack('corrupted_signal_created.wav')
+frame_rate = track.getframerate()
+track_frames = track.getnframes()
+
+print(track_frames)
