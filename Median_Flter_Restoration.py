0fc96751614f6f245614d20e5bd8c266d66d41e2
radiobenzene <aurangabadkaruditangshu@gmail.com>
2022-11-22 12:56:14 +0000

    Add function to read matlab file

--- Median_Flter_Restoration.py
+++ Median_Flter_Restoration.py
@@ -1,7 +1,9 @@
 #Author - Uditangshu Aurangabadkar
 import wave
 import matplotlib.pyplot as plot
-
+from tqdm import tqdm
+import time
+from scipy.io import loadmat
 #Function to read an audio track
 '''
     Params - track_name - specifies the name of the track to be read
@@ -18,8 +20,23 @@ def readTrack(track_name):
 '''
 def plotTrack(track):
     pass
+
+#Function to read matlab ones and zeros
+def readIndicator(file_name):
+    matlab_file = loadmat(file_name)
+    return matlab_file
+
+
 track = readTrack('corrupted_signal_created.wav')
 frame_rate = track.getframerate()
 track_frames = track.getnframes()
+indicator = readIndicator('threshold_bk.mat')
+
+#Progress Bar
+#for i in tqdm(range(100), desc= "Processing audio", ncols = 75):
+#   time.sleep(0.1)
+
+con_list = [[element for element in upperElement] for upperElement in indicator['thress']]
 
 print(track_frames)
+print()
