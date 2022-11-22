7852c97a2cd5311e8b226782c370104b327a9470
radiobenzene <aurangabadkaruditangshu@gmail.com>
2022-11-22 16:37:26 +0000

    Remove unwanted lines

--- Median_Flter_Restoration.py
+++ Median_Flter_Restoration.py
@@ -7,14 +7,19 @@ from scipy.io import loadmat
 import pandas as pd
 from mat4py import loadmat
 import numpy as np
+import scipy.io.wavfile as wav
+from scipy.io import wavfile
+import soundfile as sf
+from playsound import playsound
 #Function to read an audio track
 '''
     Params - track_name - specifies the name of the track to be read
     Return - track that has been read as an object type
 '''
 def readTrack(track_name):
-    obj = wave.open(track_name, 'r')
-    return obj
+    fs, signal = wavfile.read(track_name)
+    #data, samplerate = sf.read(track_name)
+    return signal
 
 #Function to plot an audio track frequency
 '''
@@ -25,27 +30,34 @@ def plotTrack(track):
     pass
 
 #Function to read matlab ones and zeros
-def readIndicator(file_name):
+def getIndicator(file_name):
     matlab_file = loadmat(file_name)
     return matlab_file
 
+#Function to read matlab file where index is a 1
+def getIndex(file_name):
+    matlab_file = loadmat(file_name)
+    return matlab_file
 
+#Get track here
 track = readTrack('corrupted_signal_created.wav')
-signal = track.readframes(-1)
-signal = np.frombuffer(signal, dtype ="int16")
 
-frame_rate = track.getframerate()
-track_frames = track.getnframes()
-indicator = readIndicator('threshold_bk.mat')
-
+indicator = getIndicator('threshold_bk.mat')
+indicator_indices = getIndex('threshold_index.mat')
 #Progress Bar
 #for i in tqdm(range(100), desc= "Processing audio", ncols = 75):
 #   time.sleep(0.1)
 
-
+#Loading the 
 threshold_indicator = loadmat('threshold_bk.mat')
-indicator_list = list(threshold_indicator.items())
-indicator_array = np.asarray(indicator_list)
-print(type(indicator_array))
+#indicator_list = list(threshold_indicator.items())
+#indicator_array = np.asarray(indicator_list)
+#print(type(indicator_array))
 #print(track.getfp())
+#playsound("corrupted_signal_created.wav")
+#print(type(threshold_indicator))
+print(np.where(track > 20_000))
+
+
+
 
