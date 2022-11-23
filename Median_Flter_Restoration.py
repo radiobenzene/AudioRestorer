4a7af3af44bd0a80b96230fb460bc79fc7123f0a
radiobenzene <aurangabadkaruditangshu@gmail.com>
2022-11-23 10:03:17 +0000

    Add functions for getting threshold indicator

--- Median_Flter_Restoration.py
+++ Median_Flter_Restoration.py
@@ -23,29 +23,12 @@ def readTrack(track_name):
     #data, samplerate = sf.read(track_name)
     return signal
 
-#Function to plot an audio track frequency
-'''
-    Params - track - object
-    Return - A plot of the audio track
-'''
-def plotTrack(track):
-    pass
-
-#Function to read matlab ones and zeros
-'''
-    Params - file_name - matlab file
-    Return - A dictionary of 0s and 1s
-'''
-def getIndicator(file_name):
-    matlab_file = loadmat(file_name)
-    return matlab_file
-
 #Function to read matlab file where index is a 1
 '''
     Params - file_name - matlab file
-    Return - A dictionary of indices where detection vector is 1
+    Return - A dictionary
 '''
-def getIndex(file_name):
+def getMatlabFile(file_name):
     matlab_file = loadmat(file_name)
     return matlab_file
 
@@ -56,29 +39,45 @@ def getIndex(file_name):
 '''
 def getArrayFromDict(dict_name):
     dict_list = list(dict_name.items()) 
-    ret_array = np.asarray(dict_list)[3][1][0]
+    ret_array = np.asarray(dict_list, dtype=object)[3][1][0]
     return ret_array
 
+#Function to get the detection index
+'''
+    Params - arr - input array
+           - item - 1 or 0
+    Return - index where the item is present in array
+'''
+def getIndex(arr, item):
+    indices = np.where(arr == item)
+    return indices
+
+#Function to set window length for the median filter
+def setWindowLength(val):
+    return val
 
 #Get track here
 track = readTrack('corrupted_signal_created.wav')
 
 #Loading matlab files here
-threshold_indicator = getIndicator('threshold_bk.mat')
-indicator_indices = getIndex('threshold_index.mat')
+threshold_indicator_mat = getMatlabFile('threshold_bk.mat')
+indicator_indices_mat = getMatlabFile('threshold_index.mat')
 
 #Progress Bar
 #for i in tqdm(range(100), desc= "Processing audio", ncols = 75):
 #   time.sleep(0.1)
 
-#Getting indicator array here
-threshold_indicator_list = list(threshold_indicator.items())
-threshold_indicator_array = np.asarray(threshold_indicator_list)[3][1][0]
+#Building an indicator array from matlab
+threshold_indicator = getArrayFromDict(threshold_indicator_mat)
 
-#Building an indicator array
-temp_arr = getArrayFromDict(threshold_indicator)
-print(temp_arr)
-#print(np.where(threshold_indicator_array == 1))
+#Getting indices where the detection array is 1
+threshold_array = getIndex(threshold_indicator, 1)
+
+#Setting window length here
+window_length = setWindowLength(3)
+
+#print(threshold_array)
+#print(window_length)
 
 
 
