fcf651790fa0caf78be32bfd18ad9556a053b21f
radiobenzene <aurangabadkaruditangshu@gmail.com>
2022-11-23 09:40:45 +0000

    Correct array conversion from dictionary

--- Median_Flter_Restoration.py
+++ Median_Flter_Restoration.py
@@ -5,13 +5,14 @@ from tqdm import tqdm
 import time
 from scipy.io import loadmat
 import pandas as pd
-from mat4py import loadmat
 import numpy as np
 import scipy.io.wavfile as wav
 from scipy.io import wavfile
 import soundfile as sf
 from playsound import playsound
 import pandas as pd
+from pymatreader import read_mat
+
 #Function to read an audio track
 '''
     Params - track_name - specifies the name of the track to be read
@@ -31,40 +32,53 @@ def plotTrack(track):
     pass
 
 #Function to read matlab ones and zeros
+'''
+    Params - file_name - matlab file
+    Return - A dictionary of 0s and 1s
+'''
 def getIndicator(file_name):
     matlab_file = loadmat(file_name)
     return matlab_file
 
 #Function to read matlab file where index is a 1
+'''
+    Params - file_name - matlab file
+    Return - A dictionary of indices where detection vector is 1
+'''
 def getIndex(file_name):
     matlab_file = loadmat(file_name)
     return matlab_file
 
+#Function to convert dictionary to array
+'''
+    Params - dict_name - dictionary type variable
+    Return - Converted dictionary as an array
+'''
+def getArrayFromDict(dict_name):
+    dict_list = list(dict_name.items()) 
+    ret_array = np.asarray(dict_list)[3][1][0]
+    return ret_array
+
+
 #Get track here
 track = readTrack('corrupted_signal_created.wav')
 
-indicator = getIndicator('threshold_bk.mat')
+#Loading matlab files here
+threshold_indicator = getIndicator('threshold_bk.mat')
 indicator_indices = getIndex('threshold_index.mat')
+
 #Progress Bar
 #for i in tqdm(range(100), desc= "Processing audio", ncols = 75):
 #   time.sleep(0.1)
 
-#Loading the 
-threshold_indicator = loadmat('threshold_bk.mat')
+#Getting indicator array here
+threshold_indicator_list = list(threshold_indicator.items())
+threshold_indicator_array = np.asarray(threshold_indicator_list)[3][1][0]
 
-#con_list = [[element for element in upperElement] for upperElement in threshold_indicator['thress']]
-
-
-indicator_list = list(threshold_indicator.items())
-indicator_array = np.asarray(indicator_list, dtype = object)
-print(indicator)
-#print(type(indicator_array))
-#print(track.getfp())
-#playsound("corrupted_signal_created.wav")
-#print(type(threshold_indicator))
-#print(type(indicator_array))
-#print(indicator_array)
-print(np.where(indicator_array == 0))
+#Building an indicator array
+temp_arr = getArrayFromDict(threshold_indicator)
+print(temp_arr)
+#print(np.where(threshold_indicator_array == 1))
 
 
 
