ff98bc6fbff312e90417bf9aacc1c9bf9eea53d6
radiobenzene <aurangabadkaruditangshu@gmail.com>
2022-11-25 16:22:06 +0000

    docs: Add help command for plot

--- functions.py
+++ functions.py
@@ -174,5 +174,6 @@ def showHelp():
     print("This is an audio restoration project which uses Median Filtering to restore a track\n")
     print("python3 <file_name> -h displays the Help Menu")
     print("python3 <file_name> -r runs the program")
-    print("python3 <file_name> -mse gives the MSE error between the audio tracks")
+    print("python3 <file_name> -m gives the MSE error between the audio tracks")
+    print("python3 <file_name> -p plots the graphs of the degraded and restored tracks")
     print("To get the MSE, you must first run the file")
