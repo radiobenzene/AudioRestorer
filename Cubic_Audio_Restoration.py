516a1043b7c099cb56bbf08ac1af98faca12e18e
radiobenzene <aurangabadkaruditangshu@gmail.com>
2022-11-28 17:37:55 +0000

    fix: Minor fixes

--- Cubic_Audio_Restoration.py
+++ Cubic_Audio_Restoration.py
@@ -68,8 +68,6 @@ if __name__ == "__main__":
 
     #Getting the argument list here
     fs_degraded, degraded_track = readTrack("new_degraded.wav")
-    #fs_median_clean, median_clean_track = readTrack("clean_median.wav")
-    #fs_cubic_clean, cubic_clean_track = readTrack("clean_cubic.wav")
     fs_original_clean, original_clean_track = readTrack("new_clean.wav")
 
     argument_list = sys.argv[1:]
