6e633e17330fdc1d0199728be43b9810105adeaa
radiobenzene <aurangabadkaruditangshu@gmail.com>
2022-11-29 12:53:20 +0000

    docs: Pep8 refactor

--- unit_test.py
+++ unit_test.py
@@ -14,11 +14,12 @@ import pandas as pd
 from pymatreader import read_mat
 from time import gmtime, strftime
 from functions import *
-import sys, getopt
+import sys
+import getopt
 from alive_progress import alive_bar
 from sklearn.metrics import mean_squared_error
 
-#The current file is for unit testing
+# The current file is for unit testing
 
 raw_list_1 = generateList(10)
 window_1 = 3
@@ -31,16 +32,19 @@ list_2 = zeroPadding(raw_list_2, window_2)
 mse_test_list_A = generateList(10)
 mse_test_list_B = generateList(10)
 
+
 class MedianTest(unittest.TestCase):
     def test_medianFilter1(self):
         user_filter = medianFilter(list_1, window_1)
         python_filter = checkerFunction(raw_list_1, window_1)
-        self.assertEqual(np.array(user_filter).all(), np.array(python_filter).all())
+        self.assertEqual(np.array(user_filter).all(),
+                         np.array(python_filter).all())
 
     def test_medianFilter2(self):
         user_filter = medianFilter(list_2, window_2)
         python_filter = checkerFunction(raw_list_2, window_2)
-        self.assertEqual(np.array(user_filter).all(), np.array(python_filter).all())
+        self.assertEqual(np.array(user_filter).all(),
+                         np.array(python_filter).all())
 
     def MSE_Test1(self):
         user_mse = getMSE(mse_test_list_A, mse_test_list_B)
