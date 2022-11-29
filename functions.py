acde0e54fa56c07a346c2da7b682646e1ef87d05
radiobenzene <aurangabadkaruditangshu@gmail.com>
2022-11-29 12:53:43 +0000

    docs: Pep8 refactor

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-#Author - Uditangshu Aurangabadkar
+# Author - Uditangshu Aurangabadkar
 import wave
 import matplotlib.pyplot as plt
 from tqdm import tqdm
@@ -26,70 +26,89 @@ from sklearn.metrics import mean_squared_error
     used in the main() function for median filtering
     and cubic interpolation
 '''
-#Defining error code for window with even length
+# Defining error code for window with even length
 WINDOW_LEN_EVEN = -1
 
-#Function to read an audio track
+# Function to read an audio track
 '''
     Params - track_name - specifies the name of the track to be read
     Return - track that has been read as an object type
 '''
+
+
 def readTrack(track_name):
     fs, signal = wavfile.read(track_name)
     #data, samplerate = sf.read(track_name)
     return fs, signal
 
-#Function to read matlab file where index is a 1
+
+# Function to read matlab file where index is a 1
 '''
     Params - file_name - matlab file
     Return - A dictionary
 '''
+
+
 def getMatlabFile(file_name):
     matlab_file = loadmat(file_name)
     return matlab_file
 
-#Function to convert dictionary to array
+
+# Function to convert dictionary to array
 '''
     Params - dict_name - dictionary type variable
     Return - Converted dictionary as an array
 '''
+
+
 def getArrayFromDict(dict_name, val):
     ret_val = dict_name[val]
     return ret_val
 
-#Function to get the detection index
+
+# Function to get the detection index
 '''
     Params - arr - input array
            - item - 1 or 0
     Return - index where the item is present in array
 '''
+
+
 def getIndex(arr, item):
     indices = np.where(arr == item)
-    
+
     indices = indices[1]
-    indices = indices[1:661871] #661500
-    
+    indices = indices[1:661871]  # 661500
+
     return indices
+
 
 def getIndexCubic(arr, item):
     indices = np.where(arr == item)
     indices = indices[1]
     return indices
 
-#Function to set window length for the median filter
+# Function to set window length for the median filter
+
+
 def setWindowLength(val):
     return val
 
-#Function to set block length for cubic spline interpolation
+# Function to set block length for cubic spline interpolation
+
+
 def setBlockLength(val):
     return val
-    
+
+
 # Function to check if the filter length is odd
 '''
     Params - filter_len - length of the filter
     Return - odd_flag - boolean variable if filter length is odd = true, else false
              Ending execution is window length is even
 '''
+
+
 def isOddLength(filter_len):
     is_odd_flag = False
 
@@ -101,9 +120,8 @@ def isOddLength(filter_len):
         print("Window size is even")
         sys.exit()
 
-        
-        
     return is_odd_flag
+
 
 # Function for zero padding of the list
 '''
@@ -111,6 +129,8 @@ def isOddLength(filter_len):
            - window_len - window length
     Return - A padded list with 0s determined by the window length and input list
 '''
+
+
 def zeroPadding(data_list, window_len):
     padded_list = data_list
 
@@ -126,8 +146,8 @@ def zeroPadding(data_list, window_len):
     else:
         padded_list = data_list
 
-
     return padded_list
+
 
 # Function for Median filtering
 '''
@@ -135,10 +155,12 @@ def zeroPadding(data_list, window_len):
            - window_len - window length
     Return - A new filter after being median filtered
 '''
+
+
 def medianFilter(data_list, window_len):
     new_array = []
 
-    #Case when the length of list is less than window length
+    # Case when the length of list is less than window length
     if (isOddLength(window_len) == False):
         return WINDOW_LEN_EVEN
 
@@ -146,26 +168,29 @@ def medianFilter(data_list, window_len):
         return data_list
 
     for i in range(len(data_list) - window_len + 1):
-        #Sorting the windowed elements
+        # Sorting the windowed elements
         sorted_list = np.sort(data_list[i: i + window_len])
 
-        #Getting the middle element
+        # Getting the middle element
         middle_index = int((window_len - 1) / 2)
 
-        #Getting elements from the sorted list
+        # Getting elements from the sorted list
         middle_elem = sorted_list[middle_index]
 
-        #Creating a new array of only middle elements
+        # Creating a new array of only middle elements
         new_array.append(middle_elem)
 
     return new_array
 
-#Function to plot the graph
+
+# Function to plot the graph
 '''
     Params - track - audio track
            - fs - sampling rate
     Return - a plotted graph
 '''
+
+
 def plotGraph(track, fs, title):
     length = track.shape[0] / fs
     time = np.linspace(0., length, track.shape[0])
@@ -175,12 +200,15 @@ def plotGraph(track, fs, title):
     plt.ylabel("Signal Amplitude")
     plt.show()
 
-#Function to get Mean Squared Error for 2 tracks
+# Function to get Mean Squared Error for 2 tracks
+
+
 def getMSE(clean_track, degraded_track):
     diff = (np.subtract(clean_track, degraded_track))
     squared = np.square(diff)
     output = squared.mean()
     return output
+
 
 # Function to use inbuilt median function
 '''
@@ -188,37 +216,45 @@ def getMSE(clean_track, degraded_track):
            - window_len - window_length
     Return - Filtered list using Median filter
 '''
+
+
 def checkerFunction(data_list, window_len):
-    out = scipy.signal.medfilt(data_list, kernel_size = window_len)
+    out = scipy.signal.medfilt(data_list, kernel_size=window_len)
     return out
 
-#Function to generate random number list of total_len size
+
+# Function to generate random number list of total_len size
 '''
     Params - length of the list
     Return - Randomly generated list
 '''
+
+
 def generateList(total_len):
     random_list = []
     for i in range(0, total_len):
-        n = random.randint(1,30)
+        n = random.randint(1, 30)
         random_list.append(n)
     return random_list
 
-#Function  to test individual functions
+
+# Function  to test individual functions
 '''
     The current function is for individual unit tests
 '''
+
+
 def individualUnitTest():
     pass_counter = 0
     fail_counter = 0
 
     total_tests = 7
 
-    #Generating first test and then checking
+    # Generating first test and then checking
     raw_list_1 = generateList(10)
     window_1 = 3
     list_1 = zeroPadding(raw_list_1, window_1)
-    if(np.array_equal(np.array(medianFilter(list_1, window_1)), checkerFunction(raw_list_1, window_1))):
+    if (np.array_equal(np.array(medianFilter(list_1, window_1)), checkerFunction(raw_list_1, window_1))):
         pass_counter = pass_counter + 1
         print("OK")
     else:
@@ -228,7 +264,7 @@ def individualUnitTest():
     raw_list_2 = generateList(12)
     window_2 = 5
     list_2 = zeroPadding(raw_list_2, window_2)
-    if(np.array_equal(np.array(medianFilter(list_2, window_2)), checkerFunction(raw_list_2, window_2))):
+    if (np.array_equal(np.array(medianFilter(list_2, window_2)), checkerFunction(raw_list_2, window_2))):
         pass_counter = pass_counter + 1
         print("OK")
     else:
@@ -238,7 +274,7 @@ def individualUnitTest():
     raw_list_3 = generateList(22)
     window_3 = 7
     list_3 = zeroPadding(raw_list_3, window_3)
-    if(np.array_equal(np.array(medianFilter(list_3, window_3)), checkerFunction(raw_list_3, window_3))):
+    if (np.array_equal(np.array(medianFilter(list_3, window_3)), checkerFunction(raw_list_3, window_3))):
         pass_counter = pass_counter + 1
         print("OK")
     else:
@@ -248,7 +284,7 @@ def individualUnitTest():
     raw_list_4 = generateList(1052)
     window_4 = 5
     list_4 = zeroPadding(raw_list_4, window_4)
-    if(np.array_equal(np.array(medianFilter(list_4, window_4)), checkerFunction(raw_list_4, window_4))):
+    if (np.array_equal(np.array(medianFilter(list_4, window_4)), checkerFunction(raw_list_4, window_4))):
         pass_counter = pass_counter + 1
         print("OK")
     else:
@@ -258,21 +294,21 @@ def individualUnitTest():
     raw_list_4 = generateList(20052)
     window_4 = 11
     list_4 = zeroPadding(raw_list_4, window_4)
-    if(np.array_equal(np.array(medianFilter(list_4, window_4)), checkerFunction(raw_list_4, window_4))):
+    if (np.array_equal(np.array(medianFilter(list_4, window_4)), checkerFunction(raw_list_4, window_4))):
         pass_counter = pass_counter + 1
         print("OK")
     else:
         fail_counter = fail_counter + 1
         print("NOT OK")
-    
-    #MSE test 1
+
+    # MSE test 1
     mse_test_list_A = generateList(10)
     mse_test_list_B = generateList(10)
 
     user_mse = getMSE(mse_test_list_A, mse_test_list_B)
     python_mse = mean_squared_error(mse_test_list_A, mse_test_list_B)
 
-    if(user_mse == python_mse):
+    if (user_mse == python_mse):
         pass_counter = pass_counter + 1
         print("OK")
     else:
@@ -284,24 +320,26 @@ def individualUnitTest():
     user_mse = getMSE(mse_test_list_C, mse_test_list_D)
     python_mse = mean_squared_error(mse_test_list_C, mse_test_list_D)
 
-    if(user_mse == python_mse):
+    if (user_mse == python_mse):
         pass_counter = pass_counter + 1
         print("OK")
     else:
         fail_counter = fail_counter + 1
         print("NOT OK")
 
-    #Displaying the passed tests 
+    # Displaying the passed tests
     print("Total tests passed:", pass_counter, "/", total_tests)
     print("Total tests failed:", fail_counter, "/", total_tests)
 
 
-#Function to initialize a new track
+# Function to initialize a new track
 def initializeNewTrack(track):
     new_track = track
     return new_track
 
-#Function to show help for median filtering
+# Function to show help for median filtering
+
+
 def showHelpForMedian():
     print("This is an audio restoration project which uses Median Filtering to restore a track\n")
     print("python3 <file_name> -h displays the Help Menu")
@@ -312,7 +350,9 @@ def showHelpForMedian():
     print("python3 <file_name> --diff gives the difference between the MSE of the restored tracks created by either ways")
     print("To get the MSE, you must first run the file")
 
-#Function to show help for cubic filtering
+# Function to show help for cubic filtering
+
+
 def showHelpForCubic():
     print("This is an audio restoration project which uses Cubic Filtering to restore a track\n")
     print("python3 <file_name> -h displays the Help Menu")
